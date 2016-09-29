6939507849f8233ee3ca7f70a1289bcf4dec4ac5
XTLi <xtli1689@163.com>
2016-09-29 12:03:56 -0700

    Finished directory API

--- services/directoryServiceImpl.js
+++ services/directoryServiceImpl.js
@@ -4,21 +4,12 @@ var db = require('../services/db');
 var User = require('../models/user');
 var directoryService = require('./interfaces/directoryService');
 
-// var sortDirectory = function(members) {
-// 	return members;
-// }
-
 class directoryServiceImpl extends directoryService {
 
 	constructor() {
 		super();
+		this.member;
 	}
-
-	// sortDirectory(members) {
-	// 	//TODO: Sort the diercotry to alphabetical order, 
-	// 	//starting with all the citizens who are online, and followed by all the citizens who are offline.
-	// 	return members;
-	// }
 
 	getDirectory() {
 		return new Promise(function(resolve, reject) {
@@ -28,12 +19,21 @@ class directoryServiceImpl extends directoryService {
 					var members = [];
 					var results = JSON.parse(JSON.stringify(result));
 					for(var i in results) {
-						members.push(results[i].user_name);
+						var member = {user_name: results[i].user_name,
+									online: results[i].online};
+						members.push(member);
 					}
-					members.sort();
-					//members = sortDirectory(members);
-					//TODO: Sort the diercotry, 
+					//Sort the diercotry to alphabetical order, 
 					//starting with all the citizens who are online, and followed by all the citizens who are offline.
+					members.sort(function(a, b) {
+						if(a.online < b.online) return 1;
+						else if(a.online > b.online) return -1;
+						else {
+							if (a.user_name < b.user_name) return -1;
+							else if(a.user_name > b.user_name) return 1;
+							return 0;
+						}
+					});
 					resolve(members);
 				}
 			})
