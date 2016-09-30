167fe9244d29513be65bcf3880b2be76387f18c5
Imre Nagi <imre.nagi2812@gmail.com>
2016-09-29 18:59:24 -0700

    Update database schema for message.

--- sql/V1__create_users_and_public_messages_table.sql
+++ sql/V1__create_users_and_public_messages_table.sql
@@ -12,14 +12,15 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 CREATE INDEX user_id_index ON users (id);
+CREATE INDEX user_name_index ON users (user_name);
 
 CREATE TABLE IF NOT EXISTS public_messages (
 	id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	sender_id INT NOT NULL,
 	message TEXT NOT NULL,
-	message_status VARCHAR(255),
-	latitude FLOAT,
-	longitude FLOAT,
+	message_status VARCHAR(255) NULL,
+	latitude FLOAT NULL,
+	longitude FLOAT NULL,
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
 	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
 	deleted_at TIMESTAMP NULL
@@ -27,3 +28,4 @@ CREATE TABLE IF NOT EXISTS public_messages (
 
 CREATE INDEX public_message_sender_id_index ON public_messages (sender_id);
 CREATE FULLTEXT INDEX public_message_message_index ON public_messages (message);
+
