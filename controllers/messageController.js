5bcc9808037ec5e818cb32c2c3f720a5250d8edb
XTLi <xtli1689@163.com>
2016-10-01 12:13:10 -0700

    added public chat handler in messageController

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -14,3 +14,17 @@ exports.retrieveAllPublicMessages = function(req, res, next) {
     })
 };
 
+exports.publicChatSocket = function(socket) {
+	console.log('A user elects to chat public');
+
+	//Update the chat box when a user post a new messaage and finish the persistence operation 
+	socket.on('post message', function(newMessage) {
+		console.log('user posts a message');
+	});
+
+	//Update the online userList when the user leave
+	socket.on('leave', function() {
+		console.log('user ends chat publicly');
+	});
+};
+
