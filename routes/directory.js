69f7b35d97b8646d8df3d7b12d87bb32b3ea38e6
Imre Nagi <imre.nagi2812@gmail.com>
2016-10-02 09:53:35 -0700

    Add middleware for authorization checking.

--- routes/directory.js
+++ routes/directory.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var directoryController = require('../controllers/directoryController');
 
+var auth = require('../middleware/authorization');
+router.use(auth.isAuthorizedRequest);
+
+
 router.use(function(req, res, next){
   res.setHeader('Content-Type', 'application/json');
   next();
