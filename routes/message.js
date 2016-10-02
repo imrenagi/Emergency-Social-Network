20ef75e71420ba5e8d48fa9900c05f240d2c6293
Imre Nagi <imre.nagi2812@gmail.com>
2016-10-02 09:55:44 -0700

    Refactor middleware of message router.

--- routes/message.js
+++ routes/message.js
@@ -2,18 +2,8 @@ var express = require('express');
 var router = express.Router();
 var messageController = require('../controllers/messageController');
 
-var isAuthorizedRequest = function (req, res, next) {
-	if (req.session.user_name) {
-		next();
-  	}
-  	else {
-    	var err = new Error('Unauthorized request');
-	  	err.status = 401;
-	  	next(err);
-  	}
-};
-
-router.use(isAuthorizedRequest);
+var auth = require('../middleware/authorization');
+router.use(auth.isAuthorizedRequest);
 
 router.use(function(req, res, next){
   res.setHeader('Content-Type', 'application/json');
