aa60ffdfa455f0f3b2b99ab254d7e7a05c92aa33
Imre Nagi <imre.nagi2812@gmail.com>
2016-10-02 09:12:49 -0700

    Update default value for users directory.

--- controllers/directoryController.js
+++ controllers/directoryController.js
@@ -5,7 +5,7 @@ var direcotryService = new direcotryServiceImpl();
 
 exports.displayDirectory = function(req, res, next) {
 	var page = req.param('page') || 1;
-	var limit = req.param('limit') || 20;
+	var limit = req.param('limit') || 100;
 	direcotryService.getDirectory(page, limit).then(function(result) {
 		res.send(JSON.stringify(result));
 	})
