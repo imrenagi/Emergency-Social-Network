bf3049df22363befb7f05bb6793afdb9b606850e
Ashu <atadkase@cmu.edu>
2016-10-02 12:32:02 -0700

    integrated welcome message to latest develop

---
+++ public/script/show_user_direc.js
@@ -0,0 +1,13 @@
+var lastUserId = 0;
+var hasMoreUsers = true;
+var isLoadingUsers = false;
+$("#userDirectory").hide();
+
+
+
+$("#welcomeModal").on("hidden.bs.modal", function () {
+    $("#userDirectory").show();
+});
+$(document).ready(function(){
+		
+});
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('join', {title: 'Emergency Social Network'});
+  res.render('welcome', {title: 'Emergency Social Network'});
 });
 
 router.get('/welcome', function(req, res) {
