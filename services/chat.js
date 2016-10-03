69b94de447e4730cb600adbabf40b631681110bc
XTLi <xtli1689@163.com>
2016-10-02 22:45:28 -0700

    Added socket.io handler for send message publicly

--- services/chat.js
+++ services/chat.js
@@ -1,3 +1,13 @@
+var express = require('express')
+  , PublicMessageServiceImpl = require('./publicMessageServiceImpl');
+var publicMessageService = new PublicMessageServiceImpl()
+
+const MESSAGE_ERROR = {
+        EMPTY_SENDER_OR_MESSAGE: 'MessageError.EmptySenderNameOrMessage',
+		UNKNOWN_ERROR: 'MessageError.UnknownError',
+		MYSQL_EXCEPTION: 'MessageError.InvalidMessageData'
+    }
+
 
 exports.onListening = function(socket) {
 
@@ -10,8 +20,28 @@ exports.onListening = function(socket) {
   	});
 
   	socket.on('send message', function(data) {
-  		
-  		io.emit('broadcast message', newMessage);
+  		var senderId = data.sender_id;
+		var message = data.message;
+		var message_status = data.message_status || 0;
+		var latitude = data.latitude || null;
+		var longitude = data.longitude || null;
+
+		if (senderId === undefined || message === undefined) {
+			var err = new Error();
+	  		err.status = 400;
+	  		err.message = MESSAGE_ERROR.EMPTY_SENDER_OR_MESSAGE;
+	  		return console.log(err);
+		}
+
+		publicMessageService.storeMessage(senderId, message, message_status, latitude, longitude)
+		.then(function(results) {
+			io.emit('broadcast message', JSON.stringify(results));
+		}).catch(function(err) {
+			var err = new Error();
+		  	err.status = 400;
+		  	err.message = MESSAGE_ERROR.MYSQL_EXCEPTION;
+		  	return console.log(err);
+		})
   	});
 
 }
