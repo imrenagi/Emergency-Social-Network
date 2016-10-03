2772190d018b799859b4e89653c851b4a534969d
Imre Nagi <imre.nagi2812@gmail.com>
2016-10-02 22:10:23 -0700

    Add get user endpoint.

--- controllers/directoryController.js
+++ controllers/directoryController.js
@@ -10,3 +10,19 @@ exports.displayDirectory = function(req, res, next) {
 		res.send(JSON.stringify(result));
 	})
 }
+
+exports.getUser = function(req, res, next) {
+	var userId = req.params.id;
+	if (userId === undefined) {
+		var err = new Error();
+	  	err.status = 400;
+	  	err.message = "Invalid request!";
+	  	next(err);
+	} else {
+		direcotryService.getUser(userId).then(function(result) {
+			res.status(200).send(JSON.stringify(result))
+		}).catch(function(err) {
+			next(err)
+		}) 
+	}
+}
