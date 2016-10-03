3150a0ec7d54382fd0da5c39ae5593e4ec66f439
Imre Nagi <imre.nagi2812@gmail.com>
2016-10-02 17:20:06 -0700

    Edit migration script.

--- sql/V1__create_users_and_public_messages_table.sql
+++ sql/V1__create_users_and_public_messages_table.sql
@@ -16,24 +16,26 @@ CREATE INDEX user_name_index ON users (user_name);
 
 CREATE TABLE IF NOT EXISTS public_messages (
 	id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	sender_id INT NOT NULL,
+	sender_id INT UNSIGNED NOT NULL,
 	message TEXT NOT NULL,
 	message_status VARCHAR(255) NULL,
 	latitude FLOAT NULL,
 	longitude FLOAT NULL,
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
 	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-	deleted_at TIMESTAMP NULL
+	deleted_at TIMESTAMP NULL,
+	FOREIGN KEY fk_sender(sender_id) REFERENCES users(id)
 );
 
+CREATE INDEX sender_id_index ON public_messages(sender_id);
 CREATE INDEX public_message_sender_id_index ON public_messages (sender_id);
 CREATE FULLTEXT INDEX public_message_message_index ON public_messages (message);
 
 -- Add index for online column in user table
 
 CREATE INDEX user_online_status ON users (online);
-ALTER TABLE public_messages MODIFY message_status TINYINT DEFAULT 1;
-ALTER TABLE users ADD status TINYINT DEFAULT 1 AFTER online ;
+ALTER TABLE public_messages MODIFY message_status TINYINT DEFAULT 0;
+ALTER TABLE users ADD status TINYINT DEFAULT 0 AFTER online ;
 
 
 
