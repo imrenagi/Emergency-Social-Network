45b420b605ecb73f4355eeea34fba496ef9550ef
Ashu <atadkase@cmu.edu>
2016-10-02 22:28:22 -0700

    Completed User Directory Script. Have a slight bug in layout. Add a span and at the end and append the button to it.

--- public/script/showUserDirectory.js
+++ public/script/showUserDirectory.js
@@ -1,10 +1,55 @@
-var lastUserId = 0;
-var hasMoreUsers = true;
-var isLoadingUsers = false;
-
-$("#welcomeModal").on("hidden.bs.modal", function () {
-    window.location = '/directory'
-});
+var lastPageId = 0;
+var hasMorePages = true;
+var isLoadingPages = false;
+var limit =  1;
+var loadMoreButton = $('<tr><td class = "text-center"><button class="loadmore" id="loadMoreButton" onclick="loadMoreUsers()">Load More</button></td></tr>');
 $(document).ready(function(){
-		
+	loadMoreUsers();
 });
+
+function loadMoreUsers()
+{
+
+    if(hasMorePages)
+    {
+	   $.get('/directory?page='+(lastPageId+1)+'&limit='+limit, function(data) 
+       {
+    	   $.each(data.users, function(index, element)
+            {    		
+    		  var iconPath;
+    		  switch(element.status)
+    		  {
+    			 case 0:
+    				    iconPath = '';
+    				    break;
+        			case 1:
+        				iconPath = '<i style="color: #9ccb19" class="fa fa-check-circle"></i>';
+        				break;
+        			case 2:
+        				iconPath = '<i style="color: #fcd116" class="fa fa-exclamation-triangle"></i>';
+        				break;
+        			case 3:
+        				iconPath = '<i style="color: #ce4844" class="fa fa-plus-square"></i>';
+        				break;
+        			default:
+        				iconPath ="";
+        				break;
+        		}
+         		$('.directorytable').append($('<tr><th>' + element.id +'</th> <td>' + element.user_name +'</td><td class="text-center statusTableIcon">'+  (element.status?(iconPath):"N/A")  + '</td><td class="text-center">' + (element.online? "Y":"N")+ '</td> <td class="text-center"> <span class="fa fa-comment-o"> </span></td> </tr>'));
+        	});
+           lastPageId = data.meta.page;
+           if(lastPageId >= data.meta.page_count)
+           {
+                hasMorePages = false;
+           }
+           if(hasMorePages)
+           {
+                $('.directorytable').append(loadMoreButton);
+           }
+           else
+           {
+                loadMoreButton.remove();
+           }
+    	});
+    }
+}
