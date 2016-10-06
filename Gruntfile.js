b0bab304f1a49a945ed71c7d07a555a2248e7360
Imre Nagi <imre.nagi2812@gmail.com>
2016-10-06 10:17:13 -0700

    Add grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg : grunt.file.readJSON('package.json'),
+		mochaTest : {
+			local: {
+				options: {
+					reporter: 'spec',
+					quiet: false,
+					clearRequireCache: false,
+					ui: 'tdd'
+				},
+				src: ['test/**/*.js']
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-mocha-test');
+
+	grunt.registerTask('default', []);
+
+	grunt.registerTask('test', ['mochaTest:local']);
+}
