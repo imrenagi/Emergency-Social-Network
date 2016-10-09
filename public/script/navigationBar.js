3114b8b6a786eb8656555f1c3d18ba6049de47d8
Binglei Du <bdu@andrew.cmu.edu>
2016-10-08 21:44:24 -0700

    [UC3: Share Status] User interface for changing the status

--- public/script/navigationBar.js
+++ public/script/navigationBar.js
@@ -1,3 +1,19 @@
+function updateStatus() {
+    switch(localStorage['STATUS']) {
+        case 1:
+        return '<i id="status" style="color: #9ccb19" class="fa fa-check-circle"></i>';
+        case 2:
+        return '<i id="status" style="color: #9ccb19" class="fa fa-exclamation-triangle"></i>';
+        case 3:
+        return '<i id="status" style="color: #9ccb19" class="fa fa-plus-square"></i>';
+    }
+    return '<i id="status" style="color: #000" class="fa fa-minus"></i>';
+}
+$(document).ready(function(){
+    $('#user-setting').append(' ' + localStorage['USER_NAME'] + ' &nbsp<i id="status"></i> <span class="caret"></span>');
+    $('#status').replaceWith(updateStatus());
+});
+
 $('#navbar-right').on('click', '#logout', function(event) {
      $.ajax({
         type: "DELETE",
