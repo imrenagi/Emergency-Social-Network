ad06eb9114e1bb63385c269669e7c8b58d50bb33
Imre Nagi <imre.nagi2812@gmail.com>
2016-10-15 08:40:54 -0700

    Fix db

--- sql/V1__create_users_and_public_messages_table.sql
+++ sql/V1__create_users_and_public_messages_table.sql
@@ -37,5 +37,20 @@ CREATE INDEX user_online_status ON users (`online`);
 ALTER TABLE public_messages MODIFY message_status TINYINT DEFAULT 0;
 ALTER TABLE users ADD status TINYINT DEFAULT 0 AFTER `online` ;
 
+alter table users add status_updated_at timestamp default current_timestamp not null;
+alter table users add latitude float;
+alter table users add longitude float;
+
+CREATE TABLE IF NOT EXISTS announcements (
+	id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	sender_id INT UNSIGNED NOT NULL,
+	message TEXT NOT NULL,
+	latitude FLOAT NULL,
+	longitude FLOAT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+	deleted_at TIMESTAMP NULL,
+	FOREIGN KEY fk_sender(sender_id) REFERENCES users(id)
+);
 
 
