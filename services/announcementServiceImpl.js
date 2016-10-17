af6e7564d8ad7997d1098cb384615b696fd1c493
Imre Nagi <imre.nagi2812@gmail.com>
2016-10-17 12:24:59 -0700

    refactor.

--- services/announcementServiceImpl.js
+++ services/announcementServiceImpl.js
@@ -4,27 +4,27 @@ var R = require('ramda');
 var dateHelper = require('../helpers/date');
 var AnnouncementService = require('./interfaces/announcementService');
 
-var format = function(result) {
-	var announcement = {
-		id: result.id,
-		sender: {
-			id: result.sender_id,
-			user_name: result.user_name
-		},
-		text: result.message,
-		timestamp: dateHelper.convertDateToTimestamp(result.created_at),
-		location: {
-			lat: result.latitude,
-			long: result.longitude
-		}
-	}
-	return announcement;
-}
-
 class AnnouncementServiceImpl extends AnnouncementService {
 
 	constructor(announcementDAO) {
 		super(announcementDAO);
+	}
+
+	format(result) {
+		var announcement = {
+			id: result.id,
+			sender: {
+				id: result.sender_id,
+				user_name: result.user_name
+			},
+			text: result.message,
+			timestamp: dateHelper.convertDateToTimestamp(result.created_at),
+			location: {
+				lat: result.latitude,
+				long: result.longitude
+			}
+		}
+		return announcement;
 	}
 
 	post(senderId, message, lat, long) {
@@ -38,15 +38,17 @@ class AnnouncementServiceImpl extends AnnouncementService {
 	}
 
 	getByAnnouncementId(id) {
+		var that = this;
 		return this.announcementDAO.getByAnnouncementId(id).then(function(result) {
-			return format(result)
+			return that.format(result)
 		});
 	}
 
 	getAllAnnouncements(lastId, limit) {
+		var that = this;
 		return this.announcementDAO.findAll(lastId, limit).then(function(results) {
 			var res = JSON.parse(JSON.stringify(results));
-			var json = R.map(result => format(result), res);
+			var json = R.map(result => that.format(result), res);
 			var output = {
 				announcements : json
 			}
