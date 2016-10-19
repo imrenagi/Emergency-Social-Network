5b9e3c4a0bd2a77db7986e1850b89335263e3523
XTLi <xtli1689@163.com>
2016-10-19 00:38:30 -0700

    changed api

--- services/privateMessageDAOImpl.js
+++ services/privateMessageDAOImpl.js
@@ -21,10 +21,11 @@ class PrivateMessageDAOImpl extends PrivateMessageDAO {
 	}
 
 	getMessagesByConversations(userId, conversations) {
+		console.log(conversations);
 		if(conversations.length == 0) {
 			return Promise.resolve([]);
 		}
-		let query = 'SELECT conversation_id, sender_id, receiver_id, sender_name, receiver_name, COUNT(*) AS unread_count FROM private_messages WHERE conversation_id in (' + conversations+ ') AND read_flag = 0 group by conversation_id';
+		let query = 'SELECT conversation_id, sender_id, receiver_id, sender_name, receiver_name, COUNT(IF (read_flag = 0, read_flag, null) ) AS unread_count FROM private_messages where conversation_id in (' + conversations +') group by conversation_id';
 		console.log(query);
 		return new Promise(function(resolve, reject) {
 			db.get().query(query, function(err, results) {
