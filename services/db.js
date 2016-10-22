d9e863919887321786f3cc7d700a69e1461c3abf
Imre Nagi <imre.nagi2812@gmail.com>
2016-10-22 13:05:34 -0700

    Update config for test.

--- services/db.js
+++ services/db.js
@@ -1,8 +1,11 @@
 var mysql = require('mysql');
 
-var DB = process.env.DB_NAME , TEST_DB = 'esn_db_test'
+var DB = process.env.DB_NAME , 
+TEST_DB = 'esn_db_test',
+CIRCLE_TEST_DB = 'circle_test'
 
 exports.MODE_TEST = 'mode_test'
+exports.MODE_CIRCLE_TEST = 'mode_circle'
 exports.MODE_PRODUCTION = 'mode_production'
 
 var state = {
@@ -10,12 +13,23 @@ var state = {
 	mode: null
 }
 
+function getDB(mode) {
+	var db = DB;
+	if (mode === exports.MODE_TEST) {
+		db = TEST_DB
+	} else if (mode === exports.MODE_CIRCLE_TEST) {
+		db = CIRCLE_TEST_DB
+	} 
+	return db;
+}
+
 exports.connect = function(mode, done) {
+	console.log(getDB(mode));
 	state.pool = mysql.createPool({
 		host: process.env.DB_HOST || 'localhost',
 		user: process.env.DB_USER || 'root',
 		password: process.env.DB_PASSWORD || '',
-		database: mode === exports.MODE_PRODUCTION ? DB : TEST_DB
+		database: getDB(mode)
 	})
 	state.mode = mode
 	done()
