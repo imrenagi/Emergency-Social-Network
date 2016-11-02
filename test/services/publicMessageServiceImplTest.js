216c4eec1c98021c4080fb7f9276ae429b2a0505
atadkase <ashutosh.tadkase@gmail.com>
2016-11-02 00:16:01 -0700

    Added second test to test suite (public msg)

--- test/services/publicMessageServiceImplTest.js
+++ test/services/publicMessageServiceImplTest.js
@@ -15,11 +15,87 @@ suite('Public Message Service Implementation Test', function(){
 	test('First Get all Messages with invalid last id should return all messages till limit', function(done){
 		var getMock = sinon.mock(db.get());
 		var limit = 10;
-		var query1 = 'SELECT pm.*, u.user_name FROM public_messages pm LEFT JOIN users u ON pm.sender_id = u.id order by pm.id desc limit ' + limit
+
+
+
+		var response = {"messages": [
+	    		{
+	      		"id": 1,
+	      		"sender": {
+        			"id": "123",
+        			"user_name": "xabi"
+      			},
+	      		"text": "this is the message",
+	      		"timestamp": 1474613458,
+	      		"status": 1,
+	      		"location": {
+	        		"lat": 37.3992766,
+	        		"long": -122.0894144
+	      			}
+	    		},
+	    		{
+	      		"id": 2,
+	      		"sender": {
+       		 		"id": "123",
+	    			"user_name": "xabi"
+	      		},
+		      	"text": "this is the message",
+		      	"timestamp": 1474613458,
+			    "status": 2,
+	      			"location": {
+	        		"lat": 37.3992766,
+	        		"long": -122.0894144
+	    			}
+    			}
+  			]
+		}
+		var query1 = 'SELECT pm.*, u.user_name FROM public_messages pm LEFT JOIN users u ON pm.sender_id = u.id order by pm.id desc limit ' + limit;
 		getMock.expects('query').once().withArgs(query1).returns(
-			Promise.resolve({})
+			Promise.resolve(response)
 		);
-		publicMessageService.getAllMessages(-1,10);
+		publicMessageService.getAllMessages(-1,10).then(function(result){
+				expect(result).to.be.eql(response);
+			}).catch(function(err){
+			done(err);
+		});
+		getMock.verify()
+		getMock.restore()
+		done();
+	})
+
+
+
+		test('Get all Messages with valid last id should return next messages till limit', function(done){
+		var getMock = sinon.mock(db.get());
+		var limit = 10;
+		var lastId = 1;
+		var query1 = 'SELECT pm.*, u.user_name FROM public_messages pm LEFT JOIN users u ON pm.sender_id = u.id where pm.id >='+ (lastId-limit) + ' and pm.id < ' + lastId + ' order by pm.id desc';
+		var response = {"messages": [
+	    		{
+	      		"id": 2,
+	      		"sender": {
+       		 		"id": "123",
+	    			"user_name": "xabi"
+	      		},
+		      	"text": "this is the message",
+		      	"timestamp": 1474613458,
+			    "status": 2,
+	      			"location": {
+	        		"lat": 37.3992766,
+	        		"long": -122.0894144
+	    			}
+    			}
+  			]
+		}
+		getMock.expects('query').once().withArgs(query1).returns(
+			Promise.resolve(response)
+		);
+		publicMessageService.getAllMessages(1,10).then(function(result){
+				expect(result).to.be.eql(response);
+				expect(2).to.be.eql(result.messages[0].id)
+			}).catch(function(err){
+			done(err);
+		});;
 		getMock.verify()
 		getMock.restore()
 		done();
