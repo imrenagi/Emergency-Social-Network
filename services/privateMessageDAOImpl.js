9e534df964a7960aec89846f6e69e86d3d068251
XTLi <xtli1689@163.com>
2016-11-21 16:01:03 -0800

    Fixed a bug in private chat

--- services/privateMessageDAOImpl.js
+++ services/privateMessageDAOImpl.js
@@ -24,7 +24,8 @@ class PrivateMessageDAOImpl extends PrivateMessageDAO {
 		if(conversations.length === 0) {
 			return Promise.resolve([]);
 		}
-		let query = 'SELECT conversation_id, sender_id, receiver_id, sender_name, receiver_name, COUNT(IF (read_flag = 0, read_flag, null) ) AS unread_count FROM private_messages where conversation_id in (' + conversations +') group by conversation_id';
+		let query = 'SELECT conversation_id, sender_id, receiver_id, sender_name, receiver_name, COUNT(IF (read_flag = 0 AND receiver_id = ' + userId + ', read_flag, null) ) AS unread_count FROM private_messages where conversation_id in (' + conversations +') group by conversation_id';
+		//let query = 'SELECT conversation_id, sender_id, receiver_id, sender_name, receiver_name, COUNT(IF (read_flag = 0, read_flag, null) ) AS unread_count FROM private_messages where conversation_id in (' + conversations +') group by conversation_id';
 		return new Promise(function(resolve, reject) {
 			db.get().query(query, function(err, results) {
 				if (err) reject(err);
