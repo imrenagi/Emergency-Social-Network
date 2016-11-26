fda5ff6b88da1b066408be856b6e15ce577a1875
Imre Nagi <imre.nagi2812@gmail.com>
2016-11-25 16:50:10 -0800

    Fix assertion

--- test/models/messageTest.js
+++ test/models/messageTest.js
@@ -7,7 +7,7 @@ suite('Message Test', function() {
 		var msg = new Message(1, 2, "message", 123123, 0, {lat : 100.0, long:102.0});
 		expect(msg.id).to.be.eql(1)
 		expect(msg.sender).to.be.eql(2)
-		expect(msg.text).to.be.eql(1)
+		expect(msg.text).to.be.eql("message")
 		expect(msg.timestamp).to.be.eql(123123)
 		expect(msg.status).to.be.eql(0)
 		expect(msg.location.lat).to.be.eql(100.0)
