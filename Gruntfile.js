c4c0b912ac2bbff2f8d3e3d9f7dddbd064444555
XTLi <xtli1689@163.com>
2016-11-29 22:58:43 -0800

    Added more tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
 					clearRequireCache: false,
 					ui: 'tdd'
 				},
-				src: ['test/services/publicMessageServiceImplTest.js']
+				src: ['test/**/*.js']
 			},
 			circleci: {
 	          options: {
