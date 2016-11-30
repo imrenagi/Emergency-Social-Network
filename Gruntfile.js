713117dfd56a1ccb40c30c70bb5ac408a8248324
Imre Nagi <imre.nagi2812@gmail.com>
2016-11-29 20:47:06 -0800

    Add more unit test

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
 					clearRequireCache: false,
 					ui: 'tdd'
 				},
-				src: ['test/**/*.js']
+				src: ['test/services/publicMessageServiceImplTest.js']
 			},
 			circleci: {
 	          options: {
